0146cce2f261aa84b1e4d71e461bd1052d53fdcd
Goji <Goji@DESKTOP-D4USM8P>
2018-11-28 00:22:00 +0700

    new commit

---
+++ src/Model/Admin.java
@@ -0,0 +1,57 @@
+
+package Model;
+
+import java.util.ArrayList;
+
+public class Admin {
+    private String name;
+    private String NIM;
+    private String username;
+    private String password;
+
+    public Admin(String name, String NIM, String username, String password) {
+        this.name = name;
+        this.NIM = NIM;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
